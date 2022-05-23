7c4865823e30e8d9f73ac4752f58294bef3f6d07
Wendi Luan <wendiluan@WendideMacBook-Air.local>
2022-05-22 22:43:23 -0700

    Fix Doc structure

--- src/doc.rs
+++ src/doc.rs
@@ -1,8 +1,17 @@
+use crate::block_store::BlockStore;
 use std::sync::Arc;
+use tokio::sync::Mutex;
 
 pub struct Doc {
     name: String,
     block_store: Arc<Mutex<BlockStore>>,
 }
 
-impl Doc {}
+impl Doc {
+    pub fn new(name: String) -> Self {
+        Doc {
+            name,
+            block_store: Arc::new(Mutex::new(BlockStore::new())),
+        }
+    }
+}
