9eae42c1402e77d772f625aa4b3dbccf976f7a25
LUUUAN <524976412@qq.com>
2022-05-31 21:43:49 -0700

    delete www

--- wasm_client/src/wasm_rpc.rs
+++ wasm_client/src/wasm_rpc.rs
@@ -43,6 +43,17 @@ pub mod wasm_service_client {
     pub struct WasmServiceClient<T> {
         inner: tonic::client::Grpc<T>,
     }
+    impl WasmServiceClient<tonic::transport::Channel> {
+        #[doc = r" Attempt to create a new client by connecting to a given endpoint."]
+        pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
+        where
+            D: std::convert::TryInto<tonic::transport::Endpoint>,
+            D::Error: Into<StdError>,
+        {
+            let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
+            Ok(Self::new(conn))
+        }
+    }
     impl<T> WasmServiceClient<T>
     where
         T: tonic::client::GrpcService<tonic::body::BoxBody>,
@@ -359,5 +370,8 @@ pub mod wasm_service_server {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             write!(f, "{:?}", self.0)
         }
+    }
+    impl<T: WasmService> tonic::transport::NamedService for WasmServiceServer<T> {
+        const NAME: &'static str = "wasm_rpc.WasmService";
     }
 }
