6810e4b739d8025ca2cff8eeeec5ebe4006f7d51
LUUUAN <524976412@qq.com>
2022-06-01 11:01:36 -0700

    Rough frontend

---
+++ wasm_client/www/src/Doc.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import MonacoEditor, { EditorDidMount } from 'react-monaco-editor';
+import * as monaco from 'monaco-editor';
+import { MonacoServices } from 'monaco-languageclient';
+import { useParams } from 'react-router-dom';
+import * as wasm from 'wasm-crdt';
+
+function Doc() {
+    const myRef = React.createRef();
+    const [connected, setConnected] = useState('Connected');
+    const [code, setCode] = useState('');
+    let { id } = useParams();
+
+    const MONACO_OPTIONS = {
+        autoIndent: 'full',
+        automaticLayout: true,
+        contextmenu: true,
+        fontFamily: 'monospace',
+        fontSize: 13,
+        lineHeight: 24,
+        hideCursorInOverviewRuler: true,
+        matchBrackets: 'always',
+        minimap: {
+            enabled: false,
+        },
+        readOnly: false,
+        scrollbar: {
+            horizontalSliderSize: 4,
+            verticalSliderSize: 18,
+        },
+    };
+
+    function handleClick() {
+        if (this.state.connected === 'Disconnect') {
+            setConnected('Connect');
+        } else {
+            setConnected('Disconnect');
+        }
+    }
+
+    function editorDidMount(editor) {
+        MonacoServices.install(monaco);
+        if (editor && editor.getModel()) {
+            const editorModel = editor.getModel();
+            if (editorModel) {
+                editorModel.setValue(code);
+            }
+            editorModel.onDidChangeContent((event) => {
+                event.changes
+                    .sort(
+                        (change1, change2) =>
+                            change2.rangeOffset - change1.rangeOffset
+                    )
+                    .forEach((change) => {
+                        // Operation here
+                        // wasm.delete_update(id, change.rangeOffset, change.rangeLength);
+                        // wasm.insert_update(id, change.rangeOffset, change.text);
+                        // setCode(wasm.to_string());
+                        console.log('CHANGE: ', change);
+                    });
+            });
+        }
+        editor.focus();
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log('INTERVAL');
+            // setCode(wasm.to_string());
+        }, 1000);
+    }, []);
+
+    return (
+        <div>
+            <MonacoEditor
+                ref={myRef}
+                width="100%"
+                height="80vh"
+                language="json"
+                theme="vs"
+                options={MONACO_OPTIONS}
+                editorDidMount={editorDidMount}
+            />
+            <button onClick={() => handleClick()}>{connected}</button>
+        </div>
+    );
+}
+
+export default Doc;
---
+++ wasm_client/www/src/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import * as wasm from 'wasm-crdt';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+
+function Login() {
+    const [docName, setDocName] = useState('');
+    const [clientID, setClientID] = useState('');
+    const [clientIP, setClientIP] = useState('');
+    const navigation = useNavigate();
+
+    function handleChangeDocName(event) {
+        setDocName(event.target.value);
+    }
+    function handleChangeID(event) {
+        setClientID(event.target.value);
+    }
+    function handleChangeIP(event) {
+        setClientIP(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(clientID);
+        wasm.register(clientID, clientIP, docName);
+        navigation(`/Doc/${clientID}`);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Document Name:
+                    <input
+                        type="text"
+                        value={docName}
+                        onChange={handleChangeDocName}
+                    />
+                </label>
+                <label>
+                    Client ID:
+                    <input
+                        type="text"
+                        value={clientID}
+                        onChange={handleChangeID}
+                    />
+                </label>
+                <label>
+                    Client IP:
+                    <input
+                        type="text"
+                        value={clientIP}
+                        onChange={handleChangeIP}
+                    />
+                </label>
+                <input type="submit" value="提交" />
+            </form>
+        </div>
+    );
+}
+
+export default Login;
